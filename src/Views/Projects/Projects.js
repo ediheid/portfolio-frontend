46050f070b085761faf66ff4a92163738d60a722
Edith-Heidmann <eheidmann@gmail.com>
2021-11-23 14:14:33 +0100

    Updated cursor:pointer on all clickable elements, setup main design in classes for project cards - ready to merge into main and test

--- src/Views/Projects/Projects.js
+++ src/Views/Projects/Projects.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import NavBar from "../../Components/NavBar/NavBar";
 // Images
 import itadakimasuMobileHome from "./static/itadakimasu-mobile-home.png";
+import itadakimasuDesktopPreview from "./static/itadakimasu-desktop-preview.png";
 // React-card-flip
 import ReactCardFlip from "react-card-flip";
 // Icons
 import { CgArrowRightO } from "react-icons/cg";
 import { CgArrowLeftO } from "react-icons/cg";
+import { RiDoubleQuotesL } from "react-icons/ri";
+import { RiDoubleQuotesR } from "react-icons/ri";
 
 const Projects = () => {
   //  React-card-flip state hook
@@ -28,21 +31,11 @@ const Projects = () => {
           design concepts.
         </p>
 
-        {/* // !!! To delete under construction once I have added more */}
-        <h2 className="under-construction">
-          Under Construction - stay tuned for more 💓{" "}
-        </h2>
+        {/* // ! Testing flip card.. */}
+        {/* // ! Breakdown into Components - every card will be different */}
+        {/* ReactCardFlip needs two children in order to function */}
 
-        {/* <div className="project-card-japanese-api all-cards">
-          <img
-            className="mobile-img-view"
-            src={itadakimasuMobileHome}
-            alt="Mobile view of Japanese API recipe search site, 'Itadakimasu, to eat and receive - a simple homepage with a search bar and a lantern illustration behind a glass card."
-          />
-        </div> */}
-
-        {/* // ? Testing flip card.. */}
-
+        {/* // ? Japanese Api Card.. */}
         <ReactCardFlip
           isFlipped={isFlipped}
           flipDirection="horizontal"
@@ -52,13 +45,15 @@ const Projects = () => {
           // True rotates the card in the same direction..
           infinite="true"
         >
-          {/* // ! Front */}
+          {/* // ?  Front */}
           <div className="project-card-japanese-api all-cards">
             <img
               className="mobile-img-view"
               src={itadakimasuMobileHome}
               alt="Mobile view of Japanese API recipe search site, 'Itadakimasu, to eat and receive - a simple homepage with a search bar and a lantern illustration behind a glass card."
             />
+
+            {/* Button to flip card for more info */}
             <button
               className="flip-button front-button"
               onClick={handleFlipClick}
@@ -67,13 +62,36 @@ const Projects = () => {
             </button>
           </div>
 
-          {/* // ! Back */}
-          <div className="project-card-japanese-api all-cards back-card">
+          {/* // ?  Back */}
+          <div className="project-card-japanese-api all-cards back-card japanese-api-back">
+            <h2 className="back-card-heading">Itadakimasu</h2>
+
+            <p className="back-card-paragraph">
+              A simple React app utilising a recipe search API, narrowed down to
+              a specific cuisine along with other parameters, conditions and
+              styling, resulting in the user being able to effortlessly search
+              for their favourite Japanese dish.
+              <span className="itadakimasu-quote-span">
+                <RiDoubleQuotesL fontSize="2rem" />
+                <i> Itadakimasu! </i>
+                <RiDoubleQuotesR />
+              </span>
+            </p>
+
+            <a
+              className="website-link"
+              href="https://epic-hypatia-0d24fe.netlify.app/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Link to site
+            </a>
+
+            {/* Button back to front of card */}
             <button
               className="flip-button back-button"
               onClick={handleFlipClick}
             >
-              {" "}
               <CgArrowLeftO />
             </button>
           </div>
