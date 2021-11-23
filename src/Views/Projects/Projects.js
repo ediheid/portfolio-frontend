0690c748465837419cf1e3ac3145b74e21c9a45d
Edith-Heidmann <eheidmann@gmail.com>
2021-11-23 12:03:25 +0100

    Added React-card-flip - testing with mobile view. Imported React-icons for for front and back7left and right

--- src/Views/Projects/Projects.js
+++ src/Views/Projects/Projects.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../Components/NavBar/NavBar";
-
-// images
+// Images
 import itadakimasuMobileHome from "./static/itadakimasu-mobile-home.png";
+// React-card-flip
+import ReactCardFlip from "react-card-flip";
+// Icons
+import { CgArrowRightO } from "react-icons/cg";
+import { CgArrowLeftO } from "react-icons/cg";
 
 const Projects = () => {
+  //  React-card-flip state hook
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  //  Handle click for card-flip
+  const handleFlipClick = (event) => {
+    event.preventDefault();
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <>
       <NavBar />
@@ -20,13 +33,51 @@ const Projects = () => {
           Under Construction - stay tuned for more 💓{" "}
         </h2>
 
-        <div className="project-card-japanese-api all-cards">
+        {/* <div className="project-card-japanese-api all-cards">
           <img
             className="mobile-img-view"
             src={itadakimasuMobileHome}
             alt="Mobile view of Japanese API recipe search site, 'Itadakimasu, to eat and receive - a simple homepage with a search bar and a lantern illustration behind a glass card."
           />
-        </div>
+        </div> */}
+
+        {/* // ? Testing flip card.. */}
+
+        <ReactCardFlip
+          isFlipped={isFlipped}
+          flipDirection="horizontal"
+          // Default speed is 0.6
+          // flipSpeedBackToFront="0.4"
+          // flipSpeedFrontToBack="0.4"
+          // True rotates the card in the same direction..
+          infinite="true"
+        >
+          {/* // ! Front */}
+          <div className="project-card-japanese-api all-cards">
+            <img
+              className="mobile-img-view"
+              src={itadakimasuMobileHome}
+              alt="Mobile view of Japanese API recipe search site, 'Itadakimasu, to eat and receive - a simple homepage with a search bar and a lantern illustration behind a glass card."
+            />
+            <button
+              className="flip-button front-button"
+              onClick={handleFlipClick}
+            >
+              <CgArrowRightO />
+            </button>
+          </div>
+
+          {/* // ! Back */}
+          <div className="project-card-japanese-api all-cards back-card">
+            <button
+              className="flip-button back-button"
+              onClick={handleFlipClick}
+            >
+              {" "}
+              <CgArrowLeftO />
+            </button>
+          </div>
+        </ReactCardFlip>
       </main>
     </>
   );
