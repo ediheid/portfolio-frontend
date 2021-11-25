55405dae425f1f10be4e874e12d704fdf01aa12a
Edith-Heidmann <eheidmann@gmail.com>
2021-11-25 15:46:36 +0100

    Added AOS to projects container

--- src/Views/Projects/Projects.js
+++ src/Views/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../../Components/NavBar/NavBar";
 // Card Components
 import ItadakimasuCard from "./ItadakimasuCard";
@@ -10,14 +10,23 @@ import { BsLink45Deg } from "react-icons/bs";
 import { MdOutlineExpandMore } from "react-icons/md";
 import { MdOutlineExpandLess } from "react-icons/md";
 
+// AOS
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 const Projects = () => {
-  // Open and toggle state is in each individual card component so the state is not changed on every toggle
+  // ! Open and toggle state is in each individual card component so the state is not changed on every toggle
+
+  // AOS functionality
+  useEffect(() => {
+    Aos.init({ duration: 1000, once: true });
+  }, []);
 
   return (
     <>
       {/* NavBar needs to be in individual view Component rather than App.js for mobile nav functionality */}
       <NavBar />
-      <main className="project-page-main-container">
+      <main data-aos="fade-down" className="project-page-main-container">
         <h1 className="project-page-heading">Projects</h1>
         <p className="projects-subheading">
           A collection of dynamic Single Page Applications, static websites, and
