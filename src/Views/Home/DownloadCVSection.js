830a3a1f89a6137dbeca1fd8e9ba61936e05fccb
Edith-Heidmann <eheidmann@gmail.com>
2021-11-26 12:55:41 +0100

    Added Donwload CV functionality across back and frontend

--- src/Views/Home/DownloadCVSection.js
+++ src/Views/Home/DownloadCVSection.js
@@ -1,7 +1,22 @@
 import React from "react";
 import cartoonBrowser from "./Static/marginalia-229.png";
 
+import Axios from "axios";
+import fileDownload from "js-file-download";
+
 const DownloadCVSection = () => {
+  const download = (event) => {
+    event.preventDefault();
+    Axios({
+      url: process.env.REACT_APP_BACKEND_URL,
+      method: "GET",
+      responseType: "blob",
+    }).then((res) => {
+      console.log(res);
+      fileDownload(res.data, "CV-Edith-Heidmann.pdf");
+    });
+  };
+
   return (
     <section className="download-cv-section">
       <div className="cartoon-browser-container">
@@ -10,6 +25,8 @@ const DownloadCVSection = () => {
           src={cartoonBrowser}
           alt="Cartoon browser window with pastel pink border containing a download link Edith's CV. Vector from https://icons8.com/illustrations/style--marginalia"
         ></img>
+
+        <button onClick={(event) => download(event)}>Download</button>
       </div>
     </section>
   );
