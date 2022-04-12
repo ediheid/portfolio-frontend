e92d488c43dd9945dbbe6bab668609e91497ad86
Edith-Heidmann <eheidmann@gmail.com>
2022-04-13 00:06:31 +0200

    Removed unused Hashlinks

--- src/Components/NavBar/MobileNavLinks.js
+++ src/Components/NavBar/MobileNavLinks.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
 import { motion } from "framer-motion";
 
 const animateFrom = { opacity: 0, y: -40 };
--- src/Components/NavBar/NavLinks.js
+++ src/Components/NavBar/NavLinks.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
 
 const NavLinks = () => {
   return (
