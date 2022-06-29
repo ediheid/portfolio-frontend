0f95fd6d9127f15516402b2ff6b04690ddce8dad
ediheid <eheidmann@gmail.com>
2022-06-29 22:55:58 +0200

    Setup api call with axios

---
+++ src/Views/WebApps/Weather-App/Services/GetWeatherData.js
@@ -0,0 +1,20 @@
+// import { response } from "express"
+
+// let baseUrl =
+//   "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature&current_weather=true&timezone=Europe%2FBerlin";
+
+
+
+// export const getWeatherData = async (setWeatherData, location) => {
+
+//   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e6a7dfd2692802a84edcdd8fa6cae4d7&units=metric`
+
+//   // try {
+//   //   const response = await fetch(baseUrl);
+//   //   const data = await response.json();
+    
+//   //   setWeatherData(data);
+//   // } catch (e) {
+//   //   console.log(e);
+//   // }
+// };
--- src/Views/WebApps/Weather-App/WeatherApp.js
+++ src/Views/WebApps/Weather-App/WeatherApp.js
@@ -1,40 +1,86 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import { getWeatherData } from "../../../Services/GetWeatherData";
+
+// import { getWeatherData } from "./Services/GetWeatherData";
 
 const WeatherApp = () => {
-  const [weatherData, setWeatherData] = useState([]);
-  const [longitude, setLongitude] = useState("");
-  const [latitude, setLatitude] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+  const [location, setLocation] = useState("");
 
-  const handleCityInput = (event) => {
-    event.preventDefault();
-    console.log("Submitted");
+  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e6a7dfd2692802a84edcdd8fa6cae4d7&units=metric`;
+
+  const getWeatherData = async (event) => {
+    if (event.key === "Enter") {
+      try {
+        axios.get(weatherUrl).then((response) => {
+          setWeatherData(response.data);
+          console.log(response.data);
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    // if(event.key === "Enter"){
+
+    // try{
+    //   fetch(weatherUrl)
+    //   .then(response => response.json())
+    //   .then(data => setWeatherData(data))
+
+    // console.log(weatherData)}
+    // catch(e){
+    //   console.log(e)
+    // }}
+
+    //   // try {
+    //   //   const response = await fetch(weatherUrl)
+    //   //   const data = await response.json();
+    //   //   setWeatherData(data);
+    //   //   console.log("test", response.data)
+    //   // } catch (e) {
+    //   //   console.log(e);
+    //   // }
+    // }
   };
 
-  useEffect(() => {
-    getWeatherData(setWeatherData);
-  }, []);
+  // useEffect(() => {
+  //   getWeatherData(setWeatherData);
+  // }, []);
 
-  console.log(weatherData.current_weather);
-
-  // let updatedWeatherData = {
-
-  // city: "Entered City" ,
-  // current: weatherData.current_weather.temperature,
-  // }
-
-  // console.log("Test", updatedWeatherData.city, updatedWeatherData.current)
+  // console.log(weatherData);
 
   return (
     <div className="weather-app-container">
-      <form onSubmit={handleCityInput}>
-        <input placeholder="Enter location.."></input>
+      <search>
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={getWeatherData}
+          placeholder="Enter location.."
+          type="text"
+        ></input>
+      </search>
 
-        <button type="submit">Search</button>
-      </form>
-
-      {/* <div>{weatherData.current_weather.temperature}</div> */}
+      <div>
+        <h1>Sydney</h1>
+      </div>
+      <div>
+        <h2>20</h2>
+      </div>
+      <div>
+        <span>Clouds</span>
+      </div>
+      <div>
+        <span>Feels like</span>
+      </div>
+      <div>
+        <span>Humidity</span>
+      </div>
+      <div>
+        <span>Wind</span>
+      </div>
     </div>
   );
 };
